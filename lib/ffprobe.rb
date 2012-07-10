5206b0c273802725d333a6d09c51161be8c829be
Markus Schirp <mbj@seonic.net>
2012-07-10 10:44:46 +0200

    Add a Open3.capture3 implementation for ruby-1.8.7

--- lib/ffprobe.rb
+++ lib/ffprobe.rb
@@ -2,6 +2,7 @@ require 'open3'
 require 'virtus'
 require 'rational'
 require 'multi_json'
+require 'backports'
 
 # Library namespace
 module FFProbe
@@ -43,7 +44,7 @@ module FFProbe
   #
   def self.data(path)
     command = COMMAND + [path]
-    stdout,stderr, status = Open3.capture3(*command)
+    stdout, stderr, status = capture3(command)
 
     unless status.to_i.zero?
       raise InvalidFileError,stderr
@@ -65,6 +66,61 @@ module FFProbe
     Parser.new(data).container
   end
   private_class_method :parse
+
+  # Capture command output
+  #
+  # @param [Array] command
+  #
+  # @return [Array]
+  #
+  # @api private
+  #
+  if Open3.respond_to?(:capture3)
+    def self.capture3(command)
+      Open3.capture3(command)
+    end
+  else
+    class_eval(<<-RUBY,__FILE__,__LINE__+1)
+      def self.capture3(command)
+        pi, po, pe = IO.pipe, IO.pipe, IO.pipe
+
+        pid = fork do
+          # child
+          pi[1].close
+          po[0].close
+          pe[0].close
+
+          STDIN.reopen(pi[0])
+          STDOUT.reopen(po[1])
+          STDERR.reopen(pe[1])
+
+          Kernel.exec(*command)
+        end
+
+        pi[0].close
+        pi[1].close
+
+        po[1].close
+        pe[1].close
+
+        stdout, stderr = po[0], pe[0]
+
+        data = Hash.new { |hash,key| hash[key]= '' }
+
+        until stdout.eof? and stderr.eof?
+          reads = Kernel.select([stdout, stderr]).first || []
+          reads.each do |reader|
+            data[reader] << reader.read
+          end
+        end
+        
+        pid,status = Process.waitpid2(pid)
+
+        [data.fetch(stdout),data.fetch(stderr),status.exitstatus]
+      end
+    RUBY
+  end
+
 end
 
 require 'ffprobe/stream'
